5ec0eee56ea3c3200ed1a053a528feda3eabfaf5
Barbour Smith <bar@makesmithtech.com>
2017-06-20 10:10:55 -0700

    stash

--- cnc_ctrl_v1/CNC_Functions.h
+++ cnc_ctrl_v1/CNC_Functions.h
@@ -382,7 +382,11 @@ void  singleAxisMove(Axis* axis, float endPos, float MMPerMin){
     
     float startingPos          = axis->read();
     float moveDist             = startingPos - endPos; //total distance to move
-	
+    
+    Serial.print("startingPos "); Serial.println(startingPos);
+    Serial.print("endPos "); Serial.println(endPos);
+    Serial.print("moveDist "); Serial.println(moveDist);
+    
     float direction            = -1* moveDist/abs(moveDist); //determine the direction of the move
     
     float stepSizeMM           = 0.01;                    //step size in mm
